3abe0239b64e7307656be0e2195ded4a3fa7c743
Radee Al Mahmood <radee.mahmood@northsouth.edu>
2022-01-28 21:40:59 +0600

    Displays Weather and Temperature

--- weather.py
+++ weather.py
@@ -2,3 +2,16 @@ import requests
 
 API_KEY = "8a1ca35af8a1917115081e7b8a10e720"
 BASE_URL ="http://api.openweathermap.org/data/2.5/weather"
+
+city=input("Enter a city name: ")
+requests_url = f"{BASE_URL}?q={city}&appid={API_KEY}"
+response = requests.get(requests_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather=data['weather'][0]['description']
+    temperature=round(data["main"]["temp"] - 273.15,2)
+    print(f'Weather: {weather}')
+    print(f'Temperature: {temperature}')
+else:
+    print("error")
