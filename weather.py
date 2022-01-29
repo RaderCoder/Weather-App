ef85e69f08616bbec715e8e0cddea6d28a132866
Radee Al Mahmood <radee.mahmood@northsouth.edu>
2022-01-29 08:25:07 +0600

    Hid my API key

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "8a1ca35af8a1917115081e7b8a10e720"
+API_KEY = "your_api_key"
 BASE_URL ="http://api.openweathermap.org/data/2.5/weather"
 
 city=input("Enter a city name: ")
